9153c003c000bfb67ff5981117f8844ce2379957
sadranyi <sadranyi@gmail.com>
2016-07-20 03:17:54 -0700

    added http module to call out to wikipedia

--- lambda/index.js
+++ lambda/index.js
@@ -1,87 +1,83 @@
 var Alexa = require('alexa-sdk'),
 moment = require('moment'),
-mtz = require('moment-timezone');
+mtz = require('moment-timezone'),
+http = require('http');
 
 var speechOut;
 var repromptSpeech;
 var cardTitle = "Hello Ghana Flash Cards";
 var cardContent = "Learn about Ghana using Alexa flash card";
 
-var imgObj = {
-    smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
-    largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
-};
-
-var states = {
-    GUESSMODE: '_GUESSMODE',
-    STARTMODE: '_STARTMODE'
-};
+var config = {
+    APP_ID : "amzn1.ask.skill.ae4eb4ed-6c4e-475d-ac16-d4a0865a28ce"
+}
 
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
+    alexa.APP_ID = config.APP_ID;
+    alexa.registerHandlers(handlers, helperHandlers);
     alexa.execute();
 };
 
 var handlers = {
-    'HelloGhanaIntent' : function(){
-        var speachOut = "Do you want to Know About Ghana";
-        var repromptSpeech = speachOut;
-        var cardTitle = "Hello Ghana Flash Cards";
-        var cardContent = "Learn about Ghana using Alexa flash card";
-        
-        this.emit(':tell', 'Welcome, I am Alexa, and I will be teaching you about Ghana. Lets play. To begin say, teach me');
-        this.emit(':ask', speachOut, speachOut);
-        this.emit(':tellWithCard', speachOut, cardTitle, cardContent, imgObj);
-    },
 
     'LaunchRequest' : function(){
-        this.emit('HelloGhanaIntent');
+        this.emit('SayHello');
     },
 
-    'NewSession' : function(){
-        speechOut = 'Welcome to learning about Ghana with flash cards. Would you like to learn more about Ghana?';
-        repromptSpeech = 'Say yes to start learning or no to quit.';
+    'WelcomeIntent' : function (params) {
+        this.emit('SayHello');
+    },
 
-        // Check if its the first time the skill has been invoked
-        if(Object.keys(this.attributes).length === 0){
-            this.attributes['endedSessionCount'] = 0;
-            this.attributes['cardsPlayed'] = 0;
-        }
-        this.handler.state = states.STARTMODE;
-
-        this.emit(':ask', speechOut, repromptSpeech);
-        this.emit(':askWithCard', speachOut, repromptSpeech, cardTitle, cardContent, imgObj);
+    'SessionEndedRequest' : function (params) {
+        speechOut = 'Ok, see you next time!';
+        this.emit(':tell', speechOut);
     },
 
     'AMAZON.HelpIntent' : function(){
-        speechOut = 'I will be teaching you about Ghana, if you want to know more say yes or say teach me.';
+        speechOut = 'I will be teaching you about Ghana, if you want to know more, say yes, or say, teach me.';
         repromptSpeech = 'Do you want to Know more?';
-
         this.emit(':tell', speechOut, speechOut);
-        this.emit(':tellWithCard', speachOut, cardTitle, cardContent, imgObj);
-
-        this.emit(':ask', repromptSpeech, repromptSpeech);
-        this.emit(':askWithCard', speachOut, repromptSpeech, cardTitle, cardContent, imgObj);
     },
 
     'TeachMeIntent': function(){
-        speechOut = 'This is the Teach me intent';
-        repromptSpeech = 'lets do this';
-
-        this.emit(':tell', speechOut, repromptSpeech);
-        this.emit(':tellWithCard', speachOut, cardTitle, cardContent, imgObj);
+        speechOut = "This is where i start telling you about Ghana";
+        this.emit(':tell', speechOut);
     },
 
-    'YesIntent' : function(){
+    'AMAZON.YesIntent' : function(){
         this.emit('TeachMeIntent');
     },
 
     'unhandled' : function(){
         speechOut = 'Sorry, I didn\'t get that. Try saying teach me';
         repromptSpeech = 'Try saying teach me';
-
         this.emit(':ask', speechOut, repromptSpeech);
-        this.emit(':askWithCard', speachOut, repromptSpeech, cardTitle, cardContent, imgObj);
+    },
+
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
+
+// All Helper Functions
+var helperHandlers = {
+    'SayHello' : function(){
+        speachOut = "Welcome, I am Alexa, and I will be teaching you about Ghana. Lets play. To begin say, teach me, or say, Stop to exit.";
+        repromptSpeech = "Do you want to Know more about Ghana?";
+        
+        this.emit(':tell', speachOut);
+    },
+
+    'GetHistory' : function(){
+
+    },
+
+    'GetCityHistory' : function(){
+
     }
 };
